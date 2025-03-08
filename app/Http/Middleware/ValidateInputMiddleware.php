f0caa216ec28648a2ab857406c574a00ecab6f93
M Ichsan <71815655+Ichsan-06@users.noreply.github.com>
2025-03-09 04:22:13 +0700

    New(feature): Penambahan Midleware Validate Input
    Penambahan yang dilakukan di middleware Validate input untuk memvalidasi kiriman data user

---
+++ app/Http/Middleware/ValidateInputMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ValidateInputMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        // Definisi aturan validasi
+        $rules = [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $request->user->id,
+            'age' => 'required|integer',
+        ];
+
+        // Lakukan validasi
+        $validator = Validator::make($request->all(), $rules);
+
+        // Jika validasi gagal, kembalikan response error
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        return $next($request);
+    }
+}
